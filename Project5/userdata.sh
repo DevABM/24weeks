70b4a8033efca6443ce6e686f0a68687e1caed4b
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2024-02-11 00:25:12 -0500

    week5

--- Project5/userdata.sh
+++ Project5/userdata.sh
@@ -7,3 +7,18 @@ chkconfig httpd on
 cd /var/www/html
 echo "<html><h1>Hello World</h1></html>" > index.html
 
+sudo su -
+yum -y install nginx
+clear
+ 
+systemctl status nginx
+systemctl start nginx
+systemctl status nginx
+ _________________________________
+netstat -ntlp
+ 
+ 
+cd /usr/share/nginx/html
+cat index.html
+echo "This is my first Website" > index.html
+cat index.html
