e8dee171f02d3b5e5ba10d76146ceba4f57e0024
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2024-02-22 00:00:20 -0500

    p7

---
+++ project7/test.sh
@@ -0,0 +1,7 @@
+sudo yum -y update
+sudo amazon-linux-extras install java-openjdk11
+java -version
+sudo su
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.2/bin/apache-tomcat-10.0.12.tar.gz
+tar xzvf apache-tomcat-10.0.12.tar.gz
+mv apache-tomcat-10.0.12 tomcat
