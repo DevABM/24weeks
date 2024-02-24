82851e2efc842f52e53a936fea82e9c379b055cb
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2024-02-23 23:05:43 -0500

    cli

--- project7/test.sh
+++ project7/test.sh
@@ -5,3 +5,5 @@ sudo su
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.2/bin/apache-tomcat-10.0.12.tar.gz
 tar xzvf apache-tomcat-10.0.12.tar.gz
 mv apache-tomcat-10.0.12 tomcat
+cd tomcat/bin
+./startup.sh
